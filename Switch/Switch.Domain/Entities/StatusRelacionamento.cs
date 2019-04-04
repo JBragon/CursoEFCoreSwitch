43fd24538a04b136959242f72bbe162551611a1a
Jordan Bragon <jordan10_aj@hotmail.com>
2019-04-03 23:22:25 -0300

    Finalizada a seção 3

---
+++ Switch/Switch.Domain/Entities/StatusRelacionamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Entities
+{
+    public class StatusRelacionamento
+    {
+
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
